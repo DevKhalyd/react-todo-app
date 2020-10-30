1b9d47d7893294ba631f0c13bcab9222fef8205d
Rolando <jrdosmil10@gmail.com>
2020-10-30 10:37:37 -0600

    Docs added

---
+++ docs/precache-manifest.e03e3f6307f68c014486c5f332c3e5b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9951c6acc1bdb7b749e78f490ec6ca59",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b2374ec0dda25d9104e",
+    "url": "/static/css/main.78f56169.chunk.css"
+  },
+  {
+    "revision": "54c710b222b51d313d9a",
+    "url": "/static/js/2.22a4c5cb.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/static/js/2.22a4c5cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b2374ec0dda25d9104e",
+    "url": "/static/js/main.1e279c69.chunk.js"
+  },
+  {
+    "revision": "f80722c26d7fbe8d2608",
+    "url": "/static/js/runtime-main.9f7fbb3e.js"
+  }
+]);
